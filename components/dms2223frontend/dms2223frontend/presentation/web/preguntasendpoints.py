92fe2736585e187e10b214cb48bf435e4df14d8d
Rober Lastras <94449326+roberLastras@users.noreply.github.com>
2022-11-20 01:45:53 +0100

    Commit 1 preguntasendpoints.py

---
+++ components/dms2223frontend/dms2223frontend/presentation/web/preguntasendpoints.py
@@ -0,0 +1,28 @@
+from typing import Text, Union
+from flask import redirect, url_for, session, render_template
+from werkzeug.wrappers import Response
+from dms2223common.data import Role
+from dms2223frontend.data.rest import AuthService
+from .webauth import WebAuth
+
+
+class PreguntasEndpoints():
+    
+    """ Monostate class responsible of handling the session web endpoint requests.
+    """
+    @staticmethod
+    def get_crear_preguntas(auth_service: AuthService) -> Union[Response, Text]:
+        """ Handles the GET requests to the home endpoint.
+
+        Args:
+            - auth_service (AuthService): The authentication service.
+
+        Returns:
+            - Union[Response,Text]: The generated response to the request.
+        """
+        if not WebAuth.test_token(auth_service):
+            return redirect(url_for('get_login'))
+        if Role.ADMINISTRATION.name not in session['roles']:
+            return redirect(url_for('get_home'))
+        name = session['user']
+        return render_template('preguntas/crear_preguntas.html', name=name, roles=session['roles'])
