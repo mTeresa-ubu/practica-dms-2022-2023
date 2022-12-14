a5066622ad8d7241ce3aeb8537ba6903a738a8dd
mTeresa-ubu <maite.quintanal@gmail.com>
2022-12-14 18:15:57 +0100

    Fallo

--- components/dms2223backend/dms2223backend/data/db/Elemento/__init__.py
+++ components/dms2223backend/dms2223backend/data/db/Elemento/__init__.py
@@ -5,7 +5,7 @@ from .comentario import Comentario
 from .elemento import Elemento
 from .pregunta import Pregunta
 from .respuesta import Respuesta
-from .Usuario import Usuario
+#from .Usuario import Usuario
 from .Feedback import Feedback
 from .Reporte import Reporte
 from .Voto import Voto
