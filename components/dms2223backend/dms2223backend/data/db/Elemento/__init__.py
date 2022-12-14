11ee05c23016336367981f47703bc19d4d67470b
mTeresa-ubu <maite.quintanal@gmail.com>
2022-12-14 18:17:19 +0100

    Fallos de importación

--- components/dms2223backend/dms2223backend/data/db/Elemento/__init__.py
+++ components/dms2223backend/dms2223backend/data/db/Elemento/__init__.py
@@ -6,9 +6,9 @@ from .elemento import Elemento
 from .pregunta import Pregunta
 from .respuesta import Respuesta
 #from .Usuario import Usuario
-from .Feedback import Feedback
-from .Reporte import Reporte
-from .Voto import Voto
+#from .Feedback import Feedback
+#from .Reporte import Reporte
+#from .Voto import Voto
 
 from .base import Base
 from .schema import Schema
