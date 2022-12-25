941f5da790a622ea6c94940d6dfca0fb73a47ee1
mTeresa-ubu <maite.quintanal@gmail.com>
2022-12-25 20:00:02 +0100

    results y resultsets actuaizados

--- components/dms2223backend/dms2223backend/logic/preguntaLogic.py
+++ components/dms2223backend/dms2223backend/logic/preguntaLogic.py
@@ -1,6 +1,20 @@
-from sqlalchemy import Integer
+from typing import List, Dict, Optional
+from sqlalchemy.orm.session import Session  # type: ignore
+from dms2223backend.data.db.schema import Schema 
+from dms2223backend.data.db.results import Pregunta
+from dms2223backend.data.db.resultsets import pregunta_res
+
+
+from dms2223backend.logic.answerlogic import AnswerLogic
+from dms2223backend.logic.exc.forbiddenoperationerror import ForbiddenOperationError
 
 
 class preguntaLogic():
-    def ejemplo() -> Integer:
-        return 0
+    
+    def create() -> dict:
+        try:
+            nueva: Pregunta = pregunta_res.create
+        except Exception as exception:
+         raise exception
+
+        return nueva
