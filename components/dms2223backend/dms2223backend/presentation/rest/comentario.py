691d2673d5250a5e082871265cda9933bad0ca35
acq1001 <acq1001@alu.ubu.es>
2022-12-29 00:43:25 +0100

    prueba a ver si esta bien

--- components/dms2223backend/dms2223backend/presentation/rest/comentario.py
+++ components/dms2223backend/dms2223backend/presentation/rest/comentario.py
@@ -5,26 +5,26 @@ from http import HTTPStatus
 from flask import current_app
 from dms2223backend.service.servicioComentario import servicioComentario
 
-def create_comment(respuestaId: int, body: Dict, token_info: Dict) -> Tuple[Union[Dict,str],int]:
+def create_comment(aid: int, body: Dict, token_info: Dict) -> Tuple[Union[Dict,str],int]:
     """ Crea un comentario
     """
     with current_app.app_context():
         usuario = token_info['user_token']['username']
-        comentario = servicioComentario.crear_comentario(current_app.db, usuario, body['body'], respuestaId, body['sentiment'])
+        comentario:Dict = servicioComentario.crear_comentario(current_app.db, usuario, body['body'], aid, body['sentiment'])
     return (comentario, HTTPStatus.OK)
 
 def get_comments(aid:int) -> Tuple[Dict,int]:
     """ Devuelve los comentarios del id de respuesta
     """
     with current_app.app_context():
-        comentarios:servicioComentario.get_comentarios(current_app.db,aid)
+        comentarios:Dict = servicioComentario.get_comentarios(current_app.db,aid)
     return (comentarios, HTTPStatus.OK)
 
 def get_comment(id:int) -> Tuple[Dict,int]:
     """ Devuelve un comentario sabiendo el id
     """
     with current_app.app_context():
-        comentario:servicioComentario.get_comentario(current_app.db,id)
+        comentario:Dict = servicioComentario.get_comentario(current_app.db,id)
     return (comentario, HTTPStatus.OK)
 
 def vote_comment(cid: int) -> Tuple[dict, int]:
--- components/dms2223backend/dms2223backend/service/servicioComentario.py
+++ components/dms2223backend/dms2223backend/service/servicioComentario.py
@@ -11,18 +11,29 @@ class servicioComentario():
     def init(self):
         self.auth_service = AuthService(apikey_secret='1234',host="172.10.1.10",port=4000)
 
-    def crear_comentario(schema:Schema, autor: str, body: str, aid: int, sentiment: Sentiment) -> Comentario:
+    def crear_comentario(schema:Schema, autor: str, body: str, aid: int, sentiment: Sentiment) -> dict:
         session: Session = schema.new_session()
         comentario = ComentarioFuncs.create(session, autor, body, aid, sentiment)
         #AQUI MAITE CREA UN DICCIONARIO Y CREO QUE NO ES NECESARIO
+        comentario:dict = {
+            "aid":comentario.aid,
+            "timestamp":comentario.timestamp,
+            "body":comentario.body,
+            "owner":{"username":comentario.username}
+        }
         schema.remove_session()
         return comentario
 
     def get_comentario(schema:Schema, id:int) -> Comentario:
         session: Session = schema.new_session()
-        comentarioADevolver = ComentarioFuncs.get_comentario(session, id)
+        com: Comentario = ComentarioFuncs.get_comentario(session, id)
+        com:dict = {
+            "aid":com.aid,
+            "title":com.title,
+            "timestamp":com.timestamp,
+        }
         schema.remove_session()
-        return comentarioADevolver
+        return com
 
     def get_comentarios(schema:Schema, aid:int) -> List[Comentario]:
         session: Session = schema.new_session()
