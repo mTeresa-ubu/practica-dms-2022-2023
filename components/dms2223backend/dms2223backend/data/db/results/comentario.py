a174a96de5fc7373462887032c7e220e75c4ca68
acq1001 <acq1001@alu.ubu.es>
2022-12-29 16:22:49 +0100

    prueba 8

--- components/dms2223backend/dms2223backend/data/db/results/comentario.py
+++ components/dms2223backend/dms2223backend/data/db/results/comentario.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import relationship
 
 from dms2223backend.data.sentiment import Sentiment
 
-from dms2223backend.data.db.results.votoCom import VotoCom
-from dms2223backend.data.db.results.reporteCom import ReporteCom
+#from dms2223backend.data.db.results.votoCom import VotoCom
+#from dms2223backend.data.db.results.reporteCom import ReporteCom
 from dms2223backend.data.db.results.resultbase import ResultBase
-from dms2223backend.data.db.results.respuesta import Respuesta
-from dms2223backend.data.db.results.reporteRes import ReporteRes
+#from dms2223backend.data.db.results.respuesta import Respuesta
+
 
 
 class Comentario(ResultBase):
@@ -56,7 +56,7 @@ class Comentario(ResultBase):
               - Dict: A dictionary with the mapping properties.
         """
         return {
-             'rel_respuestas': relationship(Respuesta, backref='comments')
+             #'rel_respuestas': relationship(Respuesta, backref='comments')
              #'rel_reportes_2': relationship(ReporteCom, backref='comments'),
              #'rel_votos2': relationship(VotoCom, backref='comments')
              
