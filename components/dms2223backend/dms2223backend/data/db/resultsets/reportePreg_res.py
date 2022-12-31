e750f7e5854623144360dddfe15ea41e954586c0
acq1001 <acq1001@alu.ubu.es>
2022-12-31 11:22:39 +0100

    arreglo reporte y ejemplos

--- components/dms2223backend/dms2223backend/data/db/resultsets/reportePreg_res.py
+++ components/dms2223backend/dms2223backend/data/db/resultsets/reportePreg_res.py
@@ -1,58 +1,37 @@
-from sqlalchemy import Enum
-from typing import Dict
-from sqlalchemy import Table, MetaData, Column
-from sqlalchemy import String, func 
-from sqlalchemy import Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import relationship 
-
-from dms2223backend.data.db.results.resultbase import ResultBase
-
-from sqlalchemy import Integer
-
-from datetime import datetime
-from dms2223backend.data.sentiment import Sentiment
+from typing import List, Dict, Optional
+from dms2223backend.data.db.results import ReportePreg
 from dms2223backend.data.reportstatus import ReportStatus
 
+from sqlalchemy.orm.session import Session  # type: ignore
+from sqlalchemy import select # type: ignore
 
 
-class ReporteRes(ResultBase):
+class ReportePregFuncs():
 
-    def __init__(self, username: str, reason: str, aid: int, status: ReportStatus):
+    @staticmethod
+    def create(session:Session, username: str, reason: str, qid: int) -> ReportePreg: #Estaba mal, mirar desde el spec lo que necesitamos
+        if not reason:
+            raise ValueError('Campo razón vacío.')
+        if not qid:
+            raise ValueError('Id reporte comentario vacío.')
+       
+        nueva = ReportePreg(username, reason, qid, ReportStatus.PENDING) #En el orden del result
 
-        self.username: str = username
-        self.reason: str = reason
-        self.aid: int = aid 
-        self.id: int
-        #self.timestamp: DateTime  = datetime.timestamp(datetime.now())
-        self.status: ReportStatus = status
+        session.add(nueva)
+        session.commit()
+        return nueva
         
     @staticmethod
-    def _table_definition(metadata: MetaData) -> Table:
-        """ Gets the table definition.
+    def get_reportePreg(session:Session, id:int) -> ReportePreg:
+        stmt = session.query(ReportePreg).where(ReportePreg.id == id).first()
+        return stmt
 
-          Args:
-              - metadata (MetaData): The database schema metadata
-                          (used to gather the entities' definitions and mapping)
-
-          Returns:
-              - Table: A `Table` object with the table definition.
-        """
-        return Table(
-              'reportsRes',
-              metadata,
-              Column('username', String(32), nullable=False),
-              Column('reason', String(350), nullable=False), #Nunca puede ser null
-              Column('aid', Integer, ForeignKey('answers.id'), nullable=False),
-              Column('id', Integer, autoincrement=True, primary_key=True), #Cada nuevo registro, +1
-              #Column('timestamp', DateTime, nullable=False),
-              Column('status', Enum(ReportStatus), nullable=False, default=ReportStatus.PENDING)
-
-        )
-    
-    __mapper_args__ = {
-        'polymorphic_identity': 'reportsRes',
-    }
-
-    
-
+    @staticmethod
+    def list_all(session: Session) -> List[ReportePreg]:
        
+        stmt = session.query(ReportePreg).all()
+        return stmt
+
+    
+
+    
