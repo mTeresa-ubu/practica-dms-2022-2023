e6f98626f936e6a689a5df380c873edfe04b469d
Rober Lastras <94449326+roberLastras@users.noreply.github.com>
2023-01-02 16:53:38 +0100

    Mejora base de datos de reportes (resultsets)

--- components/dms2223backend/dms2223backend/data/db/resultsets/reporteCom_res.py
+++ components/dms2223backend/dms2223backend/data/db/resultsets/reporteCom_res.py
@@ -9,21 +9,21 @@ from sqlalchemy import select # type: ignore
 class ReporteComFuncs():
 
     @staticmethod
-    def create(session:Session, username: str, reason: str, cid: int) -> ReporteCom: #Estaba mal, mirar desde el spec lo que necesitamos
+    def create(session:Session, username: str, reason: str, cid: int) -> ReporteCom:
         if not reason:
             raise ValueError('Campo razón vacío.')
         if not cid:
             raise ValueError('Id reporte comentario vacío.')
        
-        nueva = ReporteCom(username, reason, cid, ReportStatus.PENDING) #En el orden del result
+        nueva = ReporteCom(username, reason, cid, ReportStatus.PENDING)
 
         session.add(nueva)
         session.commit()
         return nueva
         
     @staticmethod
-    def get_reporteCom(session:Session, id:int) -> ReporteCom:
-        stmt = session.query(ReporteCom).where(ReporteCom.id == id).first()
+    def get_reporteCom(session:Session, crid:int) -> ReporteCom:
+        stmt = session.query(ReporteCom).where(ReporteCom.id == crid).first()
         return stmt
 
     @staticmethod
