29037574272ff80ecdf321f8a6348908034fd015
Rober Lastras <94449326+roberLastras@users.noreply.github.com>
2023-01-03 13:41:12 +0100

    Actualización del presentation rest de reporte

--- components/dms2223backend/dms2223backend/presentation/rest/reporte.py
+++ components/dms2223backend/dms2223backend/presentation/rest/reporte.py
@@ -5,6 +5,9 @@ from http import HTTPStatus
 from flask import current_app
 from sqlalchemy.orm.session import Session # type: ignore
 from dms2223backend.service.servicioReportes import ServicioReporte
+from dms2223backend.service.servicioRespuestas import ServicioRespuestas
+from dms2223backend.service.servicioComentario import servicioComentario
+from dms2223backend.service.serviciopreguntas import PreguntasServicio
 
 # Rober Lastras
 
@@ -22,26 +25,34 @@ def get_repCom(crid:int) -> Tuple[Dict,int]:
     """
     with current_app.app_context():
         rep: Dict = ServicioReporte.get_reportCom(current_app.db, crid)
-        if not rep:
-            return("No existe ese reporte", HTTPStatus.BAD_REQUEST)
     return (rep, HTTPStatus.OK)
 
-def create_repCom(cid: int, body: Dict, token_info: Dict) -> Tuple[Union[Dict,str],int]:
+def create_repCom(cid: int, body: Dict, token_info: Dict) -> Tuple[Dict,int]:
     """ Recoge los datos de la peticion y los manda al servicio de reportes (comentarios)
     """
     with current_app.app_context():
-        username = token_info['user_token']['username']
-        rep: Dict = ServicioReporte.create_reportCom(current_app.db, username, body['reason'], cid)
-    return (rep, HTTPStatus.CREATED)
+        if not servicioComentario.existe_comentario(current_app.db, cid):
+            idComentario = str(cid)
+            rep = "El comentario con id "+ idComentario +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            username = token_info['user_token']['username']
+            rep: Dict = ServicioReporte.create_reportCom(current_app.db, username, body['reason'], cid)
+            return (rep, HTTPStatus.CREATED)
 
-def change_status_repCom(crid: int, body: Dict) -> Tuple[Union[Dict,str],int]:
+def change_status_repCom(crid: int, body: Dict) -> Tuple[Dict,int]:
     """Cambia el estado de un reporte de comentario
     """
     with current_app.app_context():
-        ServicioReporte.change_status_reportCom(current_app.db, crid, body['status'])
-        rep: Dict = ServicioReporte.get_reportCom(current_app.db, crid)
-        #rep: Dict = ServicioReporte.change_status_reportCom(current_app.db, crid, body['status'])
-    return (rep, HTTPStatus.NO_CONTENT)
+        if not ServicioReporte.existe_reporteCom(current_app.db, crid):
+            idReporteCom = str(crid)
+            rep = "El reporte de comentario con id "+ idReporteCom +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            ServicioReporte.change_status_reportCom(current_app.db, crid, body['status'])
+            rep: Dict = ServicioReporte.get_reportCom(current_app.db, crid)
+            #rep: Dict = ServicioReporte.change_status_reportCom(current_app.db, crid, body['status'])
+            return (rep, HTTPStatus.OK)
 
 
 # Reportes para preguntas
@@ -58,26 +69,34 @@ def get_repPreg(qrid:int) -> Tuple[Dict,int]:
     """
     with current_app.app_context():
         rep: Dict = ServicioReporte.get_reportPreg(current_app.db, qrid)
-        if not rep:
-            return("No existe ese reporte", HTTPStatus.BAD_REQUEST)
     return (rep, HTTPStatus.OK)
 
-def create_repPreg(qid: int, body: Dict, token_info: Dict) -> Tuple[Union[Dict,str],int]:
+def create_repPreg(qid: int, body: Dict, token_info: Dict) -> Tuple[Dict,int]:
     """ Recoge los datos de la peticion y los manda al servicio de reportes (preguntas)
     """
     with current_app.app_context():
-        username = token_info['user_token']['username']
-        rep: Dict = ServicioReporte.create_reportPreg(current_app.db, username, body['reason'], qid)
-    return (rep, HTTPStatus.CREATED)
+        if not PreguntasServicio.existe_pregunta(current_app.db, qid):
+            idPregunta = str(qid)
+            rep = "La pregunta con id "+ idPregunta +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            username = token_info['user_token']['username']
+            rep: Dict = ServicioReporte.create_reportPreg(current_app.db, username, body['reason'], qid)
+            return (rep, HTTPStatus.CREATED)
 
-def change_status_repPreg(qrid: int, body: Dict) -> Tuple[Union[Dict,str],int]:
+def change_status_repPreg(qrid: int, body: Dict) -> Tuple[Dict,int]:
     """Cambia el estado de un reporte de pregunta
     """
     with current_app.app_context():
-        ServicioReporte.change_status_reportPreg(current_app.db, qrid, body['status'])
-        rep: Dict = ServicioReporte.get_reportPreg(current_app.db, qrid)
-        #rep: Dict = ServicioReporte.change_status_reportPreg(current_app.db, qrid, body['status'])
-    return rep, HTTPStatus.NO_CONTENT
+        if not ServicioReporte.existe_reportePreg(current_app.db, qrid):
+            idReportePreg = str(qrid)
+            rep = "El reporte de pregunta con id "+ idReportePreg +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            ServicioReporte.change_status_reportPreg(current_app.db, qrid, body['status'])
+            rep: Dict = ServicioReporte.get_reportPreg(current_app.db, qrid)
+            #rep: Dict = ServicioReporte.change_status_reportPreg(current_app.db, qrid, body['status'])
+            return (rep, HTTPStatus.OK)
 
 
 # Reportes para respuestas
@@ -94,23 +113,31 @@ def get_repRes(arid:int) -> Tuple[Dict,int]:
     """
     with current_app.app_context():
         rep: Dict = ServicioReporte.get_reportRes(current_app.db, arid)
-        if not rep:
-            return("No existe ese reporte", HTTPStatus.BAD_REQUEST)
     return (rep, HTTPStatus.OK)
 
-def create_repRes(aid: int, body: Dict, token_info: Dict) -> Tuple[Union[Dict,str],int]:
+def create_repRes(aid: int, body: Dict, token_info: Dict) -> Tuple[Dict,int]:
     """ Recoge los datos de la peticion y los manda al servicio de reportes (respuestas)
     """
     with current_app.app_context():
-        username = token_info['user_token']['username']
-        rep: Dict = ServicioReporte.create_reportRes(current_app.db, username, body['reason'], aid)
-    return (rep, HTTPStatus.CREATED)
+        if not ServicioRespuestas.existe_respuesta(current_app.db, aid):
+            idRespuesta = str(aid)
+            rep = "La respuesta con id "+ idRespuesta +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            username = token_info['user_token']['username']
+            rep: Dict = ServicioReporte.create_reportRes(current_app.db, username, body['reason'], aid)
+            return (rep, HTTPStatus.CREATED)
 
-def change_status_repRes(arid: int, body: Dict) -> Tuple[Union[Dict,str],int]:
+def change_status_repRes(arid: int, body: Dict) -> Tuple[Dict,int]:
     """Cambia el estado de un reporte de respuesta
     """
     with current_app.app_context():
-        ServicioReporte.change_status_reportRes(current_app.db, arid, body['status'])
-        rep: Dict = ServicioReporte.get_reportRes(current_app.db, arid)
-        #rep: Dict = ServicioReporte.change_status_reportRes(current_app.db, arid, body['status'])
-    return rep, HTTPStatus.NO_CONTENT
+        if not ServicioReporte.existe_reporteRes(current_app.db, arid):
+            idReporteRes = str(arid)
+            rep = "El reporte de respuesta con id "+ idReporteRes +" no existe"
+            return (rep, HTTPStatus.NOT_FOUND)
+        else:
+            ServicioReporte.change_status_reportRes(current_app.db, arid, body['status'])
+            rep: Dict = ServicioReporte.get_reportRes(current_app.db, arid)
+            #rep: Dict = ServicioReporte.change_status_reportRes(current_app.db, arid, body['status'])
+            return (rep, HTTPStatus.OK)
