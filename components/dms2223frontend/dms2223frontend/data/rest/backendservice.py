907df117e211e28d337eeae7d76049d8a8c6eb0c
Rober Lastras <94449326+roberLastras@users.noreply.github.com>
2023-01-05 05:17:03 +0100

    Parte de reportes en el backendservice

--- components/dms2223frontend/dms2223frontend/data/rest/backendservice.py
+++ components/dms2223frontend/dms2223frontend/data/rest/backendservice.py
@@ -4,6 +4,7 @@
 import requests
 from dms2223common.data import Role
 from dms2223common.data.rest import ResponseData
+from flask import current_app
 
 
 class BackendService():
@@ -113,3 +114,186 @@ class BackendService():
         else:
             response_data.add_message(response.content.decode('ascii'))
         return response_data
+
+    # REPORTE COMENTARIOS
+
+    def get_reportesCom(self, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.get(
+            self.__base_url() + f'/comments/reports',
+            headers={
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        current_app.logger.info(response)
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
+    def create_reporteCom(self, cid: str, reason: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.post(
+            self.__base_url() + f'/comments/{cid}/reports',
+            json = {
+                'reason': reason
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
+    def put_reporteCom(self, crid: int, status: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.put(
+            self.__base_url() + f'/comments/reports/{crid}',
+            json = {
+                'status': status
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return 
+
+
+    # REPORTE PREGUNTAS
+
+    def get_reportesPreg(self, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.get(
+            self.__base_url() + f'/questions/reports',
+            headers={
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        current_app.logger.info(response)
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
+    def create_reportePreg(self, qid: str, reason: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.post(
+            self.__base_url() + f'/questions/{qid}/reports',
+            json = {
+                'reason': reason
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+
+    def put_reportePreg(self, qrid: int, status: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.put(
+            self.__base_url() + f'/questions/reports/{qrid}',
+            json = {
+                'status': status
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
+
+    # REPORTE RESPUESTAS
+
+    def get_reportesRes(self, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.get(
+            self.__base_url() + f'/answers/reports',
+            headers={
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        current_app.logger.info(response)
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
+    def create_reporteRes(self, aid: str, reason: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.post(
+            self.__base_url() + f'/answers/{aid}/reports',
+            json = {
+                'reason': reason
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+
+    def put_reporteRes(self, arid: int, status: str, token: str):
+        response_data: ResponseData = ResponseData()
+        response: requests.Response = requests.put(
+            self.__base_url() + f'/answers/reports/{arid}',
+            json = {
+                'status': status
+            },
+            headers= {
+                'Authorization': f'Bearer {token}',
+                self.__apikey_header: self.__apikey_secret
+            },
+            timeout=60
+        )
+        response_data.set_successful(response.ok)
+        if response_data.is_successful():
+            response_data.set_content(response.json())
+        else:
+            response_data.add_message(response.content.decode('ascii'))
+        return response_data
+    
